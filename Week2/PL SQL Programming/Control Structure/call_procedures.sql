a1c1552eab2aa5e529fd456caf67bf327339c94a
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-06-28 15:20:41 +0530

    Add files via upload

---
+++ Week2/PL SQL Programming/Control Structure/call_procedures.sql
@@ -0,0 +1,18 @@
+
+-- Call procedure to apply interest discount
+BEGIN
+  apply_discount;
+END;
+/
+
+-- Call procedure to promote VIP customers
+BEGIN
+  promote_to_vip;
+END;
+/
+
+-- Call procedure to print reminders
+BEGIN
+  loan_reminders;
+END;
+/
