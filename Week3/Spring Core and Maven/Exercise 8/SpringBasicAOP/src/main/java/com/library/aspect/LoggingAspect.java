5d5cb4c4d8ceaede8f05a98300d95f9075fca659
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-03 21:13:54 +0530

    Add files via upload

---
+++ Week3/Spring Core and Maven/Exercise 8/SpringBasicAOP/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,21 @@
+package com.library.aspect;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.library.service.*.*(..))")
+    public void logBefore() {
+        System.out.println("Logging BEFORE method execution.");
+    }
+
+    @After("execution(* com.library.service.*.*(..))")
+    public void logAfter() {
+        System.out.println("Logging AFTER method execution.");
+    }
+}
