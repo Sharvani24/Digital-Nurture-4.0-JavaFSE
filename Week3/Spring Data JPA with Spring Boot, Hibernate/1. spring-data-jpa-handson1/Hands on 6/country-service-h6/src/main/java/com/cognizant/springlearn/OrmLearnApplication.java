3ab295c95268d51213dad33df178c9db27fe7c06
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-04 13:33:07 +0530

    Add files via upload

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/1. spring-data-jpa-handson1/Hands on 6/country-service-h6/src/main/java/com/cognizant/springlearn/OrmLearnApplication.java
@@ -0,0 +1,43 @@
+package com.cognizant.springlearn;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @PostConstruct
+    public void testMethods() {
+        getCountryTest();
+    }
+
+    private void getCountryTest() {
+        LOGGER.info("Start");
+        try {
+            Country country = countryService.findCountryByCode("IN");
+            LOGGER.debug("Country: {}", country);
+        } catch (CountryNotFoundException e) {
+            LOGGER.error("Error: {}", e.getMessage());
+        }
+        LOGGER.info("End");
+    }
+}
