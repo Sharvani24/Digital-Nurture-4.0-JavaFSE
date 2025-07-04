42c8fcd83740fbcfadfc6d7076a334080bb4d8d0
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-04 11:12:16 +0530

    Add files via upload

---
+++ Week1/Design principles & Patterns/Command Pattern/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Light light = new Light();
+
+        Command lightOn = new LightOnCommand(light);
+        Command lightOff = new LightOffCommand(light);
+
+        RemoteControl remote = new RemoteControl();
+
+        System.out.println("Turning ON the light:");
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        System.out.println("Turning OFF the light:");
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
