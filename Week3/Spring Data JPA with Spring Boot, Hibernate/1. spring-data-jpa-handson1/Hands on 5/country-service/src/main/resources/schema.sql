ff43db4cd0d89ec4bc5a3f8dcf4cce447781aa87
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-04 13:08:28 +0530

    Add files via upload

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/1. spring-data-jpa-handson1/Hands on 5/country-service/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DELETE FROM country;
+INSERT INTO country (co_code, co_name) VALUES ("AF", "Afghanistan");
+INSERT INTO country (co_code, co_name) VALUES ("AL", "Albania");
+INSERT INTO country (co_code, co_name) VALUES ("DZ", "Algeria");
+INSERT INTO country (co_code, co_name) VALUES ("AS", "American Samoa");
+INSERT INTO country (co_code, co_name) VALUES ("AD", "Andorra");
+INSERT INTO country (co_code, co_name) VALUES ("AO", "Angola");
+-- (Truncated for brevity; full list can be inserted by user)
