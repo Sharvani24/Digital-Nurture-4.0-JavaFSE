4dcf484bcf4db1e02a8553edbf760987a7807a44
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-04 19:06:08 +0530

    Add files via upload

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/1. spring-data-jpa-handson1/Hands on 7/add-county/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,22 @@
+package com.cognizant.ormlearn.service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryService {
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+    public Country findCountryByCode(String code) {
+        return countryRepository.findByCode(code);
+    }
+}
