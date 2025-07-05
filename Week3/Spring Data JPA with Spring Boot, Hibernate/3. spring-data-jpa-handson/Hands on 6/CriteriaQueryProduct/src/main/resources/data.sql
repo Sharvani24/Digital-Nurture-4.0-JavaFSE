0c659c2bd23e7be15ac223e1b85d846032605ae7
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-05 15:37:11 +0530

    Add files via upload

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/3. spring-data-jpa-handson/Hands on 6/CriteriaQueryProduct/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (id, name, ram, hdd, cpuSpeed, os) VALUES
+(1, 'Laptop A', 8, 512, 2.5, 'Windows'),
+(2, 'Laptop B', 16, 256, 3.0, 'Linux'),
+(3, 'Laptop C', 8, 1024, 3.2, 'Windows'),
+(4, 'Laptop D', 4, 512, 2.2, 'macOS');
