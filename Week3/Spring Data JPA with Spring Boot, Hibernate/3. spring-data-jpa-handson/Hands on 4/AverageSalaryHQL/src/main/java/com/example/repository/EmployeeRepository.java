d2aa3bf0b2f6aa4545f652aaebdeb25241c0c0c0
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-05 14:54:00 +0530

    Add files via upload

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/3. spring-data-jpa-handson/Hands on 4/AverageSalaryHQL/src/main/java/com/example/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.example.repository;
+
+import com.example.model.Employee;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+    @Query("SELECT AVG(e.salary) FROM Employee e")
+    double getAverageSalary();
+
+    @Query("SELECT AVG(e.salary) FROM Employee e WHERE e.department.id = :id")
+    double getAverageSalary(@Param("id") int id);
+}
