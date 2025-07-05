20d018d95e2a1fbd941ac5f52eefafccafc43227
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-05 15:12:05 +0530

    Add files via upload

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/3. spring-data-jpa-handson/Hands on 5/NativeQueryEmployee/src/main/java/com/example/model/Department.java
@@ -0,0 +1,28 @@
+package com.example.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employeeList;
+
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public List<Employee> getEmployeeList() { return employeeList; }
+    public void setEmployeeList(List<Employee> employeeList) { this.employeeList = employeeList; }
+}
