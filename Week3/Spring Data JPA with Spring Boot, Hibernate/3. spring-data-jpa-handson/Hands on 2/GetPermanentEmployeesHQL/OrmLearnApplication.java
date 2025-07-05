36718333c729ffab1b4f01828b7e04e754c50fc4
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-05 14:23:22 +0530

    Add files via upload

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/3. spring-data-jpa-handson/Hands on 2/GetPermanentEmployeesHQL/OrmLearnApplication.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import com.example.model.Employee;
+import com.example.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        testGetAllPermanentEmployees();
+    }
+
+    public void testGetAllPermanentEmployees() {
+        LOGGER.info("Start");
+        List<Employee> employees = employeeService.getAllPermanentEmployees();
+        LOGGER.debug("Permanent Employees: {}", employees);
+        employees.forEach(e -> LOGGER.debug("Skills: {}", e.getSkillList()));
+        LOGGER.info("End");
+    }
+}
