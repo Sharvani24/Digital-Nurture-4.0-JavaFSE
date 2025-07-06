034ed3d8e9fe2aed8b96069d274a53cb74a3965f
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-06 12:45:21 +0530

    Delete Week2/TDD using JUnit5 and Mockito/JUnit_Advanced Testing exercices/Exception Testing /src/main/java/com/example directory

--- Week2/TDD using JUnit5 and Mockito/JUnit_Advanced Testing exercices/Exception Testing /src/main/java/com/example/ExceptionThrower.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class ExceptionThrower {
-    public void throwException(boolean shouldThrow) {
-        if (shouldThrow) {
-            throw new IllegalArgumentException("Expected exception was thrown.");
-        }
-    }
-}
