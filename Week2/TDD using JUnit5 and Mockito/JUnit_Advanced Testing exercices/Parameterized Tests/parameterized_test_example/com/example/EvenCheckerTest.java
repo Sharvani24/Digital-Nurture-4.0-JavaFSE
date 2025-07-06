d4c8b0d15af82bb9dd3793a3fe70df3d21416003
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-06 11:54:52 +0530

    Add files via upload

---
+++ Week2/TDD using JUnit5 and Mockito/JUnit_Advanced Testing exercices/Parameterized Tests/parameterized_test_example/com/example/EvenCheckerTest.java
@@ -0,0 +1,23 @@
+
+package com.example;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EvenCheckerTest {
+
+    EvenChecker checker = new EvenChecker();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10, 12})
+    public void testIsEvenWithEvenNumbers(int number) {
+        assertTrue(checker.isEven(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 9, 11})
+    public void testIsEvenWithOddNumbers(int number) {
+        assertFalse(checker.isEven(number));
+    }
+}
