dac12fd6f9cde04b068f776ee953ebfaf315be55
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-06 20:01:35 +0530

    Add files via upload

---
+++ Week2/TDD using JUnit5 and Mockito/Junit_Spring Test Exercise/Exercise 8/controller-exception-test/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
