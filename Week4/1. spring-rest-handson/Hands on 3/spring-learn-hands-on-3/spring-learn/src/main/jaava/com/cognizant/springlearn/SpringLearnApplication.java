79d2274a73f58a66abf9f23f4ce62247834a5c02
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-07 13:47:32 +0530

    Add files via upload

---
+++ Week4/1. spring-rest-handson/Hands on 3/spring-learn-hands-on-3/spring-learn/src/main/jaava/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,38 @@
+
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting SpringLearnApplication...");
+        SpringApplication.run(SpringLearnApplication.class, args);
+        displayDate();
+        LOGGER.info("SpringLearnApplication finished execution.");
+    }
+
+    public static void displayDate() {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+        try {
+            Date parsedDate = format.parse("31/12/2018");
+            LOGGER.debug("Parsed Date: {}", parsedDate.toString());
+        } catch (Exception e) {
+            LOGGER.error("Error parsing date: {}", e.getMessage());
+        }
+        LOGGER.info("END");
+    }
+}
