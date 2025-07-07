fb2892ac499c5e002239463e8d2e7693913136e9
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-07 20:07:42 +0530

    Add files via upload

---
+++ Week4/2. spring-rest-handson/REST - Get all countries/CountryController.java
@@ -0,0 +1,27 @@
+package com.cognizant.springlearn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.Country;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        LOGGER.info("START: getAllCountries()");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> countries = (List<Country>) context.getBean("countryList");
+        LOGGER.info("END: getAllCountries()");
+        return countries;
+    }
+}
