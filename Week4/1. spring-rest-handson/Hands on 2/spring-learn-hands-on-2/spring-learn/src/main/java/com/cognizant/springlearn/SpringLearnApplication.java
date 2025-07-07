70c44f84a5a829895cde4bd3913565d9e7fd2b3b
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-07 10:49:54 +0530

    Add files via upload

---
+++ Week4/1. spring-rest-handson/Hands on 2/spring-learn-hands-on-2/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,36 @@
+
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting SpringLearnApplication...");
+        SpringApplication.run(SpringLearnApplication.class, args);
+        displayDate();
+        LOGGER.info("SpringLearnApplication finished execution.");
+    }
+
+    public static void displayDate() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+        try {
+            Date parsedDate = format.parse("31/12/2018");
+            System.out.println("Parsed Date: " + parsedDate);
+        } catch (Exception e) {
+            System.out.println("Error parsing date: " + e.getMessage());
+        }
+    }
+}
