be6ccb009a0a0bf811bb699dfa0840710a0475b6
Sharvani Rathore <sharvanirathore2004@gmail.com>
2025-07-10 11:34:57 +0530

    Add files via upload

---
+++ Week4/4. spring-rest-handson/Implement REST DELETE Service /employee-rest-service-updated/src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.employee.controller;
+
+import com.example.employee.exception.EmployeeNotFoundException;
+import com.example.employee.model.Employee;
+import com.example.employee.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @PutMapping
+    public void updateEmployee(@RequestBody @Valid Employee employee) throws EmployeeNotFoundException {
+        service.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) throws EmployeeNotFoundException {
+        service.deleteEmployee(id);
+    }
+}
